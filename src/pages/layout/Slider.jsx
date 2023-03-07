ab4b8c0653eef60c78bdb7d48f74c6ce0ad3526c
italeenamk <italeenamk@gmail.com>
2023-03-07 14:01:00 +0500

    Slider content complete
    Slider complete

--- src/pages/layout/Slider.jsx
+++ src/pages/layout/Slider.jsx
@@ -1,5 +1,7 @@
 import './style/slider.css';
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Slider = () => {
@@ -22,7 +24,21 @@ const Slider = () => {
                               </div>
                           </div>
                       </div>
-                      <div className="col-md-6 col-sm-12 text-right">hi</div>
+                      <div className="col-md-6 colum-right-content col-sm-12 text-right">
+                          <div className="slider-right-content">
+                              <div className="slider-right-dott-content">
+                                  <span className="slider-right-span-content"></span>
+                                  <span></span>
+                                  <span></span>
+                                  <span></span>
+                                  <span></span>
+                              </div>
+                              <div className="slider-left-right-content">
+                                  <FontAwesomeIcon icon={ faArrowLeft } className="fa-1x" />
+                                  <FontAwesomeIcon icon={ faArrowRight } className="fa-2x" />
+                              </div>
+                          </div>
+                      </div>
                   </div>
               </div>
           </section>
